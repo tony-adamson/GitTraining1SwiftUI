95c40708594cbebaddf148a5676f80a2499652ae
Anton <antony.adamson@gmail.com>
2023-03-24 14:14:00 +0600

    Сделали Navigation View Переход через ссылку

--- GitTraining1SwiftUI/ContentView.swift
+++ GitTraining1SwiftUI/ContentView.swift
@@ -8,14 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var isPresented = false
     var body: some View {
         NavigationView {
-            Button("Show Modal") {
-                self.isPresented = true
-            }.foregroundColor(.red)
-            .sheet(isPresented: $isPresented) {
-                ModalView()
+            NavigationLink(destination: ModalView()) {
+                Text("Go Next")
             }
             .navigationBarTitle("Xcode and Git")
         }
--- GitTraining1SwiftUI/ModalView.swift
+++ GitTraining1SwiftUI/ModalView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct ModalView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Thi is the Second View")
+        }.navigationBarTitle("Second View", displayMode: .inline)
     }
 }
 
