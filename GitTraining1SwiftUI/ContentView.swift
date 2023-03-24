0450147554dd872b12670a8dd94e317236f8c3ab
Anton <antony.adamson@gmail.com>
2023-03-24 14:18:50 +0600

    Цвет ссылки перехода изменен на зеленый в ветке мейн

--- GitTraining1SwiftUI/ContentView.swift
+++ GitTraining1SwiftUI/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
         NavigationView {
             NavigationLink(destination: ModalView()) {
                 Text("Go Next")
-            }.foregroundColor(.red)
+            }.foregroundColor(.green)
             .navigationBarTitle("Xcode and Git")
         }
     }
