3a42a34ba237837841d28fd50342974d43a20e65
Anton <antony.adamson@gmail.com>
2023-03-24 14:34:26 +0600

    Удален ВиСтак после ошибочного слияния

--- GitTraining1SwiftUI/ContentView.swift
+++ GitTraining1SwiftUI/ContentView.swift
@@ -10,13 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationView {
-            VStack {
-                Text("This is a Content View")
                 NavigationLink(destination: ModalView()) {
                     Text("Go Next")
                 }.foregroundColor(.purple)
                     .navigationBarTitle("Xcode and Git")
-            }
         }
     }
 }
